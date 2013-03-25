d6226c39eab497effdba5aa0c95620e2c758d33f
Dan Radez <dradez@redhat.com>
2013-03-25 15:04:26 -0400

    updating the references to the controller to use the floating IP address

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -4,20 +4,20 @@ class trystack::compute(){
     # Configure Nova
     nova_config{
         'auto_assign_floating_ip': value => 'True';
-        #"network_host": value => "${controller_node_public}";
+        #"network_host": value => "${pacemaker_priv_floating_ip}";
         "network_host": value => "$::ipaddress";
         "libvirt_inject_partition": value => "-1";
-        #"metadata_host": value => "$controller_node_public";
+        #"metadata_host": value => "$pacemaker_priv_floating_ip";
         "metadata_host": value => "$::ipaddress";
-        "qpid_hostname": value => "$controller_node_public";
+        "qpid_hostname": value => "$pacemaker_priv_floating_ip";
         "rpc_backend": value => "nova.rpc.impl_qpid";
         "multi_host": value => "True";
     }
 
     class { 'nova':
-        sql_connection       => "mysql://nova:${nova_db_password}@${controller_node_public}/nova",
+        sql_connection       => "mysql://nova:${nova_db_password}@${pacemaker_priv_floating_ip}/nova",
         image_service        => 'nova.image.glance.GlanceImageService',
-        glance_api_servers   => "http://$controller_node_public:9292/v1",
+        glance_api_servers   => "http://$pacemaker_priv_floating_ip:9292/v1",
         verbose              => $verbose,
     }
 
@@ -38,14 +38,14 @@ class trystack::compute(){
 
     class {"nova::compute":
         enabled => true,
-        vncproxy_host => "$controller_node_public",
+        vncproxy_host => "$pacemaker_priv_floating_ip",
         vncserver_proxyclient_address => "$ipaddress",
     }
 
     class { 'nova::api':
         enabled           => true,
         admin_password    => "$nova_user_password",
-        auth_host         => "$controller_node_public",
+        auth_host         => "$pacemaker_priv_floating_ip",
     }
 
     class { 'nova::network':
