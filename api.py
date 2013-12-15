a86f7043fe09cb78d09fb48bbc4c97843a71e952
Dan Radez <dradez@redhat.com>
2013-12-15 10:47:32 -0500

    adding comments to the api file

--- api.py
+++ api.py
@@ -1,3 +1,36 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+
+###################
+# To use this script:
+# 1. change the 0 to 1 on the line “if 0″ by the comment “# generate”
+# 2. run ‘python api.py’ this will generate a trystack.cfg file with empty values
+# 3. switch the 1 back to a 0
+# 4. edit the trystack.cfg file with appropriate values
+# 5. edit the user and password and url around line 40 to point to foreman
+# 6. run ‘python api.py’
+# 7. repeat 5 & 6 as nessesary
+##################
+
+
+
 import ConfigParser
 import json
 import base64
