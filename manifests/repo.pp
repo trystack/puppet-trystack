9827095c4d763cc2a7c0e8a18afc01512d9ea2fe
Tim Rozet <tdrozet@gmail.com>
2015-02-11 11:20:51 -0500

    Added minor check to see if osfamily is redhat before trying to yum install repo.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,12 +1,11 @@
 class trystack::repo {
-    file {'/etc/hosts':
-      source=> 'puppet:///modules/trystack/etc.hosts',
-    }
-
+  if $::osfamily == 'RedHat' {
     yumrepo { "openstack-juno":
       baseurl => "http://repos.fedorapeople.org/repos/openstack/openstack-juno/epel-7/",
       descr => "RDO Community repository",
       enabled => 1,
       gpgcheck => 0,
-   }
+    }
+  }
+
 }
