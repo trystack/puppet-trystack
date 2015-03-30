5b9af18095a31bbd2854b66c1a7cb3640e46040b
Tim Rozet <tdrozet@gmail.com>
2015-03-30 09:44:33 -0400

    Added ceph repos to repolist
    This is needed for ceph to install correctly.  Needed for cinder
    backend.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -17,5 +17,25 @@ class trystack::repo {
       enabled => 1,
       gpgcheck => 0,
     }
+
+    yumrepo { 
+            "ceph":
+                   baseurl => "http://ceph.com/rpm-giant/el7/\$basearch",
+                   descr => "Ceph packages for \$basearch",
+                   enabled => 1,
+                   gpgcheck => 0;
+            "Ceph-noarch":
+                   baseurl => "http://ceph.com/rpm-giant/el7/noarch",
+                   descr => "Ceph noarch packages",
+                   enabled => 1,
+                   gpgcheck => 0;
+            "ceph-source":
+                   baseurl => "http://ceph.com/rpm-giant/el7/SRPMS",
+                   descr => "Ceph source packages",
+                   enabled => 1,
+                   gpgcheck => 0;
+    }
+
+    
   }
 }
