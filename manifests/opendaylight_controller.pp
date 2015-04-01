5899a5332ca80a6574d6c357352b647af7527d02
Tim Rozet <tdrozet@gmail.com>
2015-04-01 12:13:56 -0400

    Syntax change to make if statment more clear

--- manifests/opendaylight_controller.pp
+++ manifests/opendaylight_controller.pp
@@ -1,5 +1,5 @@
 class trystack::opendaylight_controller {
-     if $odl_rest_port == '' { $odl_rest_port= '8081'}
+     if !$odl_rest_port { $odl_rest_port = '8081'}
      class {"opendaylight":
        extra_features => ['odl-base-all', 'odl-aaa-authn', 'odl-restconf', 'odl-nsf-all', 'odl-adsal-northbound', 'odl-mdsal-apidocs', 'odl-ovsdb-openstack', 'odl-ovsdb-northbound', 'odl-dlux-core'],
        odl_rest_port  => $odl_rest_port,
