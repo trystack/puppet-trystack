58a4aee28a2bab8f0bcd1b5b8c1ecc9ceb3d78f1
Tim Rozet <tdrozet@gmail.com>
2015-04-08 14:35:43 -0400

    Adds python-rados which now replaces python ceph + includes NTP
    Can get ntp here: puppet module install puppetlabs-ntp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,20 @@ class trystack {
         command => '/usr/sbin/setenforce 0',
         unless => '/usr/sbin/getenforce | grep Permissive',
     }
+    
     include stdlib
     stage { 'presetup':
       before => Stage['setup'],
+    }
+    class { '::ntp':
+     stage => presetup,
     }
 
     class { "trystack::repo":
      stage => presetup,
     }
-
+    ->
+    package { "python-rados":
+     ensure => latest,
+    } 
 }
